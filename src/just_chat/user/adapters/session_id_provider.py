8606d505e0781ae03a9d1da10faa82797b5119a0
Vladimir Kibisov <kryseyt@gmail.com>
2024-11-20 22:06:58 +0300

    Add comment

--- src/just_chat/user/adapters/session_id_provider.py
+++ src/just_chat/user/adapters/session_id_provider.py
@@ -4,6 +4,13 @@ from just_chat.user.application.interfaces.session_gateway import SessionToken
 from just_chat.user.domain.user import UserId
 
 
+# Он нужен в частности для того, чтобы прокидывать token в вызов интерактора GetUserIdByToken
+# Вызвать сам интерактор получения по токену мы не можем, ибо:
+# - Нам придется передавать токен при вызове, а в месте вызова мы не должны знать ни о каком токене и об аутенфикации вообще
+# ИЛИ
+# - Нам придется прокидывать токен в конструктор интерактора, но тогда мы не сможем применять один интерактор к нескольким
+# токенам - только к одному, который и прокинули в конструктор. Ну или делать интерактору метод set_token,
+# короче какой-то хуйней заниматься, лучше просто сделать обертку над всем этим аутенфикации в виде IdProvider
 class SessionIdProvider(IdProvider):
     def __init__(self, token: SessionToken, user_id_provider: GetUserIdByToken) -> None:
         self._token = token
